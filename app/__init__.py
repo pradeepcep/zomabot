8451e974adfe92e1fef97840b768f5e8d027709c
Pradeep CE <breathingcode@gmail.com>
2017-03-01 16:26:15 +0530

    Receive events on the webhook

--- app/__init__.py
+++ app/__init__.py
@@ -6,9 +6,13 @@ app = Flask(__name__)
 
 # Some tokens and keys used by the app.
 app.config['VERIFY_TOKEN'] = '<you could hard-code your token here>'
-# Or set an environment variable to override it.
+app.config['PAGE_TOKEN'] = '<you could hard-code your page token here>'
+
+# Or set environment variables to override them.
 if os.environ.get('VERIFY_TOKEN'):
     app.config['VERIFY_TOKEN'] = os.environ.get('VERIFY_TOKEN')
+if os.environ.get('PAGE_TOKEN'):
+    app.config['PAGE_TOKEN'] = os.environ.get('PAGE_TOKEN')
 
 
 @app.route('/')
@@ -16,8 +20,17 @@ def index():
     return 'Zomabot!'
 
 
-@app.route('/webhook', methods=['GET', 'POST'])
+# GET request to the webhook is used for authentication.
+@app.route('/webhook', methods=['GET'])
 def webhook():
     if request.args.get('hub.verify_token', '') == app.config['VERIFY_TOKEN']:
         return request.args.get('hub.challenge')
     return 'Authentication failed', 403
+
+
+# POST request to the webhook is used for events.
+@app.route('/webhook', methods=['POST'])
+def events():
+    # For now, just log that we received something.
+    print "Received a POST request!\n"
+    return '', 200
