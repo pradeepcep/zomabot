acafb544904ad02af5a2bf58388d0868e0ed4aea
Pradeep CE <breathingcode@gmail.com>
2017-03-01 15:10:22 +0530

    Basic webhook implementation

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,14 @@
 from flask import *
+import os
 
 app = Flask(__name__)
+
+
+# Some tokens and keys used by the app.
+app.config['VERIFY_TOKEN'] = '<you could hard-code your token here>'
+# Or set an environment variable to override it.
+if os.environ.get('VERIFY_TOKEN'):
+    app.config['VERIFY_TOKEN'] = os.environ.get('VERIFY_TOKEN')
 
 
 @app.route('/')
@@ -8,6 +16,8 @@ def index():
     return 'Zomabot!'
 
 
-@app.route('/webhook')
+@app.route('/webhook', methods=['GET', 'POST'])
 def webhook():
-    return 'Zomabot webhook!'
+    if request.args.get('hub.verify_token', '') == app.config['VERIFY_TOKEN']:
+        return request.args.get('hub.challenge')
+    return 'Authentication failed', 403
